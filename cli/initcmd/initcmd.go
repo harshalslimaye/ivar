d981c28fc187fba8e5e22dbd14b876896fe4dcb6
harshal-limaye <harshal.limaye.in@gmail.com>
2024-05-09 17:44:02 +0530

    moving messages to initcmd file

--- cli/initcmd/initcmd.go
+++ cli/initcmd/initcmd.go
@@ -6,11 +6,22 @@ import (
 	"strings"
 
 	"github.com/harshalslimaye/ivar/internal/helper"
-	"github.com/harshalslimaye/ivar/internal/messages"
 	"github.com/harshalslimaye/ivar/internal/packagejson"
 	"github.com/logrusorgru/aurora"
 	"github.com/spf13/cobra"
 )
+
+const initCmdAlreadyExists = `
+Oops! Found a package.json file here already. 
+If you want to start fresh, just delete it and 
+run 'ivar init' again.
+`
+
+const initCmdWelcome = `
+%s init walks you through creating a %s file with the essentials, suggesting handy defaults. 
+For more info, type 'ivar help init'. Remember, you can press ^C anytime to quit. 
+Afterward, use 'ivar install <pkg>' to add dependencies. Easy peasy!
+`
 
 func InitCmd() *cobra.Command {
 	var yesFlag bool
@@ -22,12 +33,12 @@ func InitCmd() *cobra.Command {
 			pkgjson := packagejson.GetNewPackageJson(yesFlag)
 
 			if packagejson.Exists() {
-				fmt.Println(aurora.Red(messages.InitCmdAlreadyExists))
+				fmt.Println(aurora.Red(initCmdAlreadyExists))
 				os.Exit(1)
 			}
 
 			if !yesFlag {
-				fmt.Printf(messages.InitCmdWelcome, aurora.Cyan("ivar"), aurora.Green("package.json"))
+				fmt.Printf(initCmdWelcome, aurora.Cyan("ivar"), aurora.Green("package.json"))
 
 				pkgjson.Name = helper.AskQuestion(`package name: %s`, helper.GetCurrentDirName())
 				pkgjson.Version = helper.AskQuestion(`version %s`, "1.0.0")
--- internal/registry/fetch-dependencies.go
+++ internal/registry/fetch-dependencies.go
@@ -17,11 +17,9 @@ type Dependency struct {
 
 func FetchDependencies(name, version string) (*Dependency, error) {
 	endpoint := fmt.Sprintf("https://registry.npmjs.org/%s/%s", name, version)
-
 	var dep Dependency
 
 	res, err := http.Get(endpoint)
-
 	if err != nil {
 		return nil, err
 	}
@@ -31,8 +29,7 @@ func FetchDependencies(name, version string) (*Dependency, error) {
 		return nil, fmt.Errorf("failed to fetch dependency %s@%s: %s", aurora.Yellow(name), aurora.Yellow(version), aurora.Yellow(res.Status))
 	}
 
-	err = json.NewDecoder(res.Body).Decode(&dep)
-	if err != nil {
+	if err := json.NewDecoder(res.Body).Decode(&dep); err != nil {
 		return nil, err
 	}
 
